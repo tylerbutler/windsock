0a8aea1124d2dc1442aaedf31b1c4afe899e4adb
Tyler Butler <tyler@tylerbutler.com>
2026-06-30 08:28:53 -0700

    Improve website quick start

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,5 +1,17 @@
 import { defineConfig } from 'astro/config';
+import expressiveCode from 'astro-expressive-code';
 
 export default defineConfig({
   site: 'https://tylerbutler.github.io/windsock',
+  integrations: [
+    expressiveCode({
+      themes: ['github-light'],
+      styleOverrides: {
+        borderRadius: '0.875rem',
+        frames: {
+          shadowColor: 'transparent',
+        },
+      },
+    }),
+  ],
 });
